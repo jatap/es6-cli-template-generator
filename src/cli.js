b5994be36d4e83c2143791afe7b7bfac06d9dfd6
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2020-03-26 16:59:42 +0000

    refactor(packages): update

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,7 @@
 
 const program = require('commander');
 
-program
-  .version('0.1.0', '-v, --version')
-  .name('XXX')
-  .description('XXX');
+program.version('0.1.0', '-v, --version').name('XXX').description('XXX');
 
 /* eslint-disable no-console */
 program.on('--help', () => {

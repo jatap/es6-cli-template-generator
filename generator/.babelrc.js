16b301cf5e84993ff60e9cebb7ac8a11544630b5
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2021-05-15 18:02:15 +0100

    refactor(core): update dependencies and simplify eslint config

--- generator/.babelrc.js
+++ generator/.babelrc.js
@@ -9,7 +9,12 @@ module.exports = {
       },
     ],
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-throw-expressions',
+  ],
   retainLines: true,
   sourceMaps: 'inline',
   ignore: ['node_modules', '.compiled'],
